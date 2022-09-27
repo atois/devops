fa0fa3deee7c388c5357c896a7082a723c7274a7
atois <atois@hotmail.com>
2022-09-27 11:37:40 +0800

    first blood

---
+++ src/main/java/com/atois/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.atois.demo.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "love is often the fruit of marriage";
+    }
+}
+
+
+
+
